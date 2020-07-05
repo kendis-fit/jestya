15092766648442e1c00993dec93e0d77eb05f21d
Kurt Endless <kendis_it@hotmail.com>
2020-07-05 19:39:09 +0300

    update: init redis microservice

--- redis/src/main.ts
+++ redis/src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from "@nestjs/microservices";
+
 import { AppModule } from './app.module';
 
+const logger = new Logger("Redis");
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+	transport: Transport.REDIS,
+	options: {
+		url: "redis://localhost:6379"
+	}
+  });
+  await app.listen(() => logger.log("Redis microservice is listening"));
 }
 bootstrap();
