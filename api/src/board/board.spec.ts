8c2f1ca6a9af7702b1ea53c9c889a05d14e91aea
Kurt Endless <kendis_it@hotmail.com>
2020-07-13 00:44:37 +0300

    update: board test

--- api/src/board/board.spec.ts
+++ api/src/board/board.spec.ts
@@ -6,14 +6,18 @@ import { BoardService } from "./board.service";
 
 describe("Board", () => {
 	let service: BoardService;
+	let findOne: jest.Mock;
 
 	beforeEach(async () => {
+		findOne = jest.fn();
 		const module: TestingModule = await Test.createTestingModule({
 			providers: [
 				BoardService,
 				{
 					provide: getRepositoryToken(Board),
-					useValue: {},
+					useValue: {
+						findOne,
+					},
 				},
 			],
 		}).compile();
@@ -24,6 +28,29 @@ describe("Board", () => {
 	describe("Board Service", () => {
 		it("should be defined", () => {
 			expect(service).toBeDefined();
+		});
+
+		describe("if board is found", () => {
+			let board: Board;
+			beforeEach(() => {
+				board = new Board();
+				findOne.mockReturnValue(board);
+			});
+
+			it("should return the board", async () => {
+				const foundBoard = await service.findById("some_id");
+				expect(foundBoard).toEqual(board);
+			});
+		});
+
+		describe("else board isn't found", () => {
+			beforeEach(() => {
+				findOne.mockReturnValue(undefined);
+			});
+
+			it("shoud throw an error", async () => {
+				await expect(service.findById("some_id")).rejects.toThrow();
+			});
 		});
 	});
 });
