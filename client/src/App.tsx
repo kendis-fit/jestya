cbef491e82bb6cad4dc30e8c1c45982725cd6b71
Kurt Endless <kendis_it@hotmail.com>
2020-07-15 15:10:30 +0300

    add: init client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./main.scss";
+import {  } from "redux";
+
+function App() {
+  return (
+    <div>
+      <button className="btn btn-primary">cool</button>
+    </div>
+  );
+}
+
+export default App;
