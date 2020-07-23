42390e939db05482bdb05f7aa02a9715697e6c36
Kurt Endless <kendis_it@hotmail.com>
2020-07-23 16:41:49 +0300

    update: fixed bug with test

--- api/src/pipes/parse-enum/parse-enum.pipe.spec.ts
+++ api/src/pipes/parse-enum/parse-enum.pipe.spec.ts
@@ -1,7 +1,8 @@
 import { ParseEnumPipe } from "./parse-enum.pipe";
+import { Role } from "src/user/user.entity";
 
 describe("ParseEnumPipe", () => {
 	it("should be defined", () => {
-		expect(new ParseEnumPipe()).toBeDefined();
+		expect(new ParseEnumPipe(Role)).toBeDefined();
 	});
 });
