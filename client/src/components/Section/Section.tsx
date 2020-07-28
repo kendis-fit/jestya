b21f45f23312a8fc25e4aa2c5ef278dd02d3d7fb
zakhar.levchuk <zakhar.levchuk@kyivland.gov.ua>
2020-07-28 17:01:44 +0300

    section layout

--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import Task from "./Task";
 
+const Colors = ["bg-primary", "bg-success", "bg-danger", "bg-warning", "bg-info"];
+
 const Section = (props: any) => {
+	const [taskList, setTaskList] = useState<any[]>([]);
+
+	const hendleAddTask = () => {
+		setTaskList([...taskList, "s"]);
+	};
+
 	return (
 		<div className="section" style={{ background: props.index % 2 === 0 ? "#fafafa" : "#f2f2f2" }}>
-			<div className={`section__header p-2 mb-3 d-flex ${props.addSection ? "bg-secondary" : "bg-primary"} `}>
+			<div
+				className={`section__header p-2 mb-3 d-flex ${
+					props.addSection ? "bg-secondary" : Colors[props.index]
+				} `}
+			>
 				{props.addSection ? (
-					<button className="btn text-white p-0 d-flex align-items-center">
+					<button className="btn text-white p-0 d-flex align-items-center" onClick={props.hendleAddSection}>
 						<span className="material-icons p-2 mr-2">add_circle_outline</span> Add Section
 					</button>
 				) : (
@@ -16,19 +28,38 @@ const Section = (props: any) => {
 					</>
 				)}
 			</div>
-			<Task />
-			<Task />
-			<button
-				className="
-				section__button
-				text-primary
-				rounded-circle
-				btn 
-				bg-white
-				shadow"
-			>
-				<span className="material-icons">add_circle_outline</span>
-			</button>
+			<div className="section__tasklist">
+				{props.addSection ? (
+					<p className="text-center">Add new section</p>
+				) : (
+					<>
+						{taskList.map((ell, i) => (
+							<Task key={i} />
+						))}
+						<button
+							className="
+							section__button
+							text-primary
+							rounded-circle
+							btn 
+							bg-white
+							shadow "
+							onClick={hendleAddTask}
+						>
+							<span className="material-icons">add_circle_outline</span>
+						</button>
+						{taskList.length === 0 ? (
+							<div className="section__noTasks">
+								<span className="noTasks__icon text-secondary  material-icons">fact_check</span>
+								<h5 className="text-center text-muted">No Tasks </h5>
+								<p className="text-center text-muted m-0 w-75">
+									Add new task by click "+" button or drag task here
+								</p>
+							</div>
+						) : null}
+					</>
+				)}
+			</div>
 		</div>
 	);
 };
--- client/src/components/SectionList/SectionList.tsx
+++ client/src/components/SectionList/SectionList.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Section from "../Section";
 
 const SectionList = () => {
 	const arr = [1, 2];
+
+	const [sectionsList, setSectionsList] = useState<any[]>([1]);
+
+	const hendleAddSection = () => {
+		setSectionsList([...sectionsList, 1]);
+	};
+
+	console.log(sectionsList);
+
 	return (
 		<div className="sectionList ">
-			{arr.map((ell, i) => (
-				<Section key={i} index={i} />
+			{sectionsList.map((ell, i) => (
+				<Section key={i} index={i} hendleAddSection={hendleAddSection} />
 			))}
 			<Section index={arr.length} addSection />
 		</div>
