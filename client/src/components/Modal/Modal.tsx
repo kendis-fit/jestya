c80efcbcc695638796785c7ab76bd2a372cb123a
Kurt Endless <kendis_it@hotmail.com>
2020-08-02 17:49:39 +0300

    add: modal for bootstrap

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,81 @@
+import bootstrap from "bootstrap";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
+
+export interface IModal {
+    title: string;
+    open: boolean;
+    size?: "xl" | "lg" | "sm";
+    children?: ReactNode;
+    isStatic?: boolean;
+    verticalCentered?: boolean;
+    fullscrean?: boolean;
+}
+
+const Modal = (props: IModal) => {
+    const [modal, setModal] = useState<bootstrap.Modal | null>(null);
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    const [fullscreen, setFullscreen] = useState<string | null>(props.fullscrean ? "modal-fullscreen" : null);
+    const [size, setSize] = useState<string | null>(props.size ?? "sm");
+    const [center, setCenter] = useState<string | null>(props.verticalCentered ? "modal-dialog-centered" : null);
+
+    useEffect(() => {
+        setFullscreen(props.fullscrean ? "modal-fullscreen" : null);
+    }, [props.fullscrean]);
+
+    useEffect(() => {
+        setSize(props.size ?? "sm");
+    }, [props.size]);
+
+    useEffect(() => {
+        setCenter(props.verticalCentered ? "modal-dialog-centered" : null);
+    }, [props.verticalCentered]);
+
+    useEffect(() => {
+        if (modalRef) {
+            const current = modalRef.current;
+            if (current) {
+                setModal(new bootstrap.Modal(current));
+            }
+        }
+    }, [modalRef]);
+
+    useEffect(() => {
+        if (modal) {
+            props.open ? modal.show() : modal.hide();
+        }
+    }, [modal, props.open]);
+
+    return(
+        <div className="modal fade" 
+            data-backdrop={props.isStatic ? "static" : null}
+            data-keyboard={props.isStatic ? "false" : null}
+            id={`${props.title}-modal`}
+            tabIndex={-1}
+            aria-labelledby={`${props.title}-modal modal-${size} ${fullscreen} ${center}`}
+            aria-hidden="true"
+            ref={modalRef}>
+            <div className={`modal-dialog modal`}>
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{props.title}</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    {
+                        props.children ? <div className="modal-body">
+                            {props.children}
+                        </div> : null
+                    }
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+declare module "bootstrap" {
+	interface IModalOptions {
+		backdrop?: boolean;
+		keyboard?: boolean;
+		focus?: boolean;
+		show?: boolean;
+	}
+
+	class Modal {
+		constructor(element: HTMLElement, options?: IModalOptions);
+		show(): void;
+		hide(): void;
+	}
+}
