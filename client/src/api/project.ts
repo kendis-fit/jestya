5b8179b0ea35b2948a35c3b726de2ebeb0991bdc
Kurt Endless <kendis_it@hotmail.com>
2020-08-03 10:40:55 +0300

    add: interface for project in api

--- client/src/api/project.ts
+++ client/src/api/project.ts
@@ -5,26 +5,74 @@ const mockObject = [
 		id: "1",
 		name: "Front-end",
 		description: "mdaa",
-		data: [12, 34, 44],
-		labels: ["to-do", "in processing", "done"],
+		boards: [
+			{
+				name: "to-do",
+				countTasks: 12,
+			},
+			{
+				name: "in processing",
+				countTasks: 34,
+			},
+			{
+				name: "done",
+				countTasks: 44,
+			},
+		],
 	},
 	{
 		id: "2",
 		name: "Back-end",
-		data: [12, 34, 44],
-		labels: ["to-do", "in processing", "done"],
+		boards: [
+			{
+				name: "to-do",
+				countTasks: 22,
+			},
+			{
+				name: "in processing",
+				countTasks: 10,
+			},
+			{
+				name: "done",
+				countTasks: 67,
+			},
+		],
 	},
 	{
 		id: "3",
 		name: "Devops",
-		data: [12, 34, 44],
-		labels: ["to-do", "in processing", "done"],
+		boards: [
+			{
+				name: "to-do",
+				countTasks: 2,
+			},
+			{
+				name: "in processing",
+				countTasks: 54,
+			},
+			{
+				name: "done",
+				countTasks: 3,
+			},
+		],
 	},
 ];
 
+export interface IBoard {
+	name: string;
+	countTasks: number;
+}
+
+export interface IProject {
+	id: string;
+	name: string;
+	description?: string;
+	boards: IBoard[];
+}
+
 const projects = {
 	findAll: () => {
-		return new Promise<any[]>(async (resolve, reject) => {
+		return new Promise<IProject[]>(async (resolve, reject) => {
 			try {
 				const req = await fetcher(`${process.env.REACT_APP_API}/projects`, {
 					method: "GET",
--- client/src/components/Project/Project.tsx
+++ client/src/components/Project/Project.tsx
@@ -3,14 +3,7 @@ import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
 import Modal from "../Modal";
-
-export interface IProject {
-    id: string;
-    name: string;
-    description?: string;
-    data: number[];
-    labels: string[];
-}
+import { IProject } from "../../api/project";
 
 const getRandomColour = () => {
     var letters = '0123456789ABCDEF';
@@ -24,7 +17,7 @@ const getRandomColour = () => {
 const Project = (props: IProject) => {
     const [openModal, setOpenModal] = useState(false);
     const [isRedirected, setIsRedirected] = useState(false);
-    const datasets = [{ data: props.data, backgroundColor: props.data.map(() => getRandomColour()) }];
+    const datasets = [{ data: props.boards.map(board => board.countTasks), backgroundColor: props.boards.map(() => getRandomColour()) }];
 
     if (isRedirected) {
         return <Redirect to={`/projects/${props.id}`} />
@@ -37,10 +30,10 @@ const Project = (props: IProject) => {
                     <div className="project">
                         <div className="project__body" onClick={() => setIsRedirected(true)}>
                             <div className="project__title"><span>{props.name}</span></div>
-                            <Pie data={{ labels: props.labels, datasets }} options={{ legend: { display: false } }} />
+                            <Pie data={{ labels: props.boards.map(board => board.name), datasets }} options={{ legend: { display: false } }} />
                         </div>
                         <div className="area area--filled">
-                            <span>Count tasks: {props.data.length}</span>
+                            <span>Count tasks: {props.boards.length}</span>
                             <span onClick={() => props.description && setOpenModal(!openModal)} className={`material-icons area__icon ${props.description ? "" : "area__icon--disabled"}`}>info</span>
                         </div>
                     </div>
