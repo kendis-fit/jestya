e1af595928e7688b6b0599a769a2efa78ab2c236
zakhar.levchuk <zakhar.levchuk@kyivland.gov.ua>
2020-08-12 16:16:33 +0300

    drag task

--- client/src/components/Boards/Boards.tsx
+++ client/src/components/Boards/Boards.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useVanillaFetch } from "vanilla-hooks";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useLayoutEffect } from "react";
 import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 
 import Error from "../Error";
@@ -42,13 +42,35 @@ const Boards = (props: IBoards) => {
 	const handleOnDragEnd = async (result: any) => {
 		setIsDragingBoard(false); //reset drag status
 		setIsDragingTask(false); //reset drag status
+
 		if (!result.destination) return; // dropped outside the list
-		const dragIndexs = {
-			startIndex: result.source.index,
-			endIndex: result.destination.index,
-		};
-		props.dragBoard(dragIndexs);
-		await resource.projects.updateBoard(projectId, result.draggableId, { position: result.destination.index });
+		// console.log("result", result);
+		// console.log(result.source.droppableId.match(/drag-board:\w+/));
+		// console.log(/drag-board\w+/.test(result.source.droppableId));
+
+		if (result.source.droppableId === "drag-board") {
+			const dragIndexs = {
+				startIndex: result.source.index,
+				endIndex: result.destination.index,
+			};
+			props.dragBoard(dragIndexs);
+			await resource.projects.updateBoard(projectId, result.draggableId, {
+				position: result.destination.index,
+			});
+		} else {
+			console.group("TASKSSS");
+			console.log(result);
+			console.log(result.source.droppableId.match(/[^_]*$/g)[0]);
+			console.groupEnd();
+			const a = {
+				dropInBoardId: result.destination.droppableId.match(/[^_]*$/g)[0],
+				dropOutBoardId: result.source.droppableId.match(/[^_]*$/g)[0],
+				dropInPosition: result.destination.index,
+				dropOutPosition: result.source.index,
+				dropTaskId: result.draggableId,
+			};
+			console.log(a);
+		}
 	};
 
 	const handleOnDragStart = (dragItem: any) => {
